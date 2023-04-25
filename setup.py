98cdd3a497b505c70a9860074347af8e7da37b29
Ioan <44036462+AgapeIoan@users.noreply.github.com>
2023-04-25 22:24:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setuptools.setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
-    install_requires=['aiohttp', 'asyncio'],
+    install_requires=['requests'],
     extras_require={
         'dev': ['check-manifest'],
         # 'test': ['coverage'],
