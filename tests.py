3a5396857695a1efa4d13a9ba6caf7ac07d15b7b
Ioan <44036462+AgapeIoan@users.noreply.github.com>
2023-05-02 14:48:26 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -50,7 +50,7 @@ class TestData(unittest.TestCase):
             print(result)
             self.assertTrue(result)
 
-        users = [" afggtgbb", ".!,)6", "gdyjgfukk", "xh hc iv"]
+        users = [" a", ".!,)6", "gdyjgfukk", "xh hc iv"]
         for user in users:
             result = wrapper.search_user(user)
             print(result)
